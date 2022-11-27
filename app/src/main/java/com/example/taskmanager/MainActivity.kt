57fe4ca48ec874da10206da887b1808aa6476018
Caíque Brener <brenercaique0806@gmail.com>
2022-11-27 11:44:37 -0300

    Create the composable splashscreen

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.taskmanager
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -30,14 +38,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TaskManagerTheme {
-        Greeting("Android")
+fun SplashScreen(imageResource: Painter, taskStatus: String, subTitle: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .wrapContentHeight(Alignment.CenterVertically)
+    ) {
+        Image(
+            painter = imageResource,
+            contentDescription = null,
+            alignment = Alignment.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = taskStatus,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(top = 24.dp, bottom = 8.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = subTitle,
+            fontSize = 16.sp,
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
     }
 }
